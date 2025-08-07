6333350ffb5c931643939523c1d9e565ac9b1407
adamleraton <adamrodrigues2001@gmail.com>
2025-08-07 14:15:23 +0200

    contact et tinymce

---
+++ fil-rouge/sprints/sprint1/david/modifications/public/js/tinymce/myTiny.js
@@ -0,0 +1,8 @@
+tinymce.init(
+    {
+        selector: 'textarea#message',
+        branding: false,
+        promotion: false,
+        language: 'fr_FR'
+    }
+)
---
+++ fil-rouge/sprints/sprint1/david/modifications/quote.js
@@ -0,0 +1,9 @@
+tinymce.init({
+  selector: '#explanation',
+  height: 300,
+  menubar: false,
+  plugins: 'lists link code',
+  toolbar: 'undo redo | bold italic underline | bullist numlist | code',
+  branding: false,
+  language: 'fr'
+});
