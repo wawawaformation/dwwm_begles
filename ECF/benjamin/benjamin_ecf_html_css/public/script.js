e6af2f56cc4bcd212b0dcc9befe3d02fcdcf2000
Ben <ben.voydie64@gmail.com>
2025-09-03 16:23:01 +0200

    rendu ecf

---
+++ ECF/benjamin/benjamin_ecf_html_css/public/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const burger = document.getElementById("menu-burger");
+    const menu = document.getElementById("menu");
+    const chapo = document.querySelector(".chapo");
+
+    burger.addEventListener("click", () => {
+        menu.classList.toggle("active");
+
+        if (menu.classList.contains("active")) {
+            chapo.style.display = "none";
+        } else {
+            chapo.style.display = "block"
+        }
+    })
+})
+
