9e4885ce5346656c47511ad4b0300bdf3b007583
Este98 <este98@hotmail.fr>
2025-09-04 08:19:02 +0200

    déplacmement du rendu html

--- ECF/esteban/public/js/burger_menu.js
+++
@@ -1,9 +0,0 @@
-/**
- * Animation du menu burger
- */
-const burgerElt = document.querySelector(".burger-button");
-const mainMenuElt = document.getElementById("nav")
-
-burgerElt.addEventListener("click", function () {
-    mainMenuElt.classList.toggle("hidden");
-});
