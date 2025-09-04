c1716e93f326bc5494c406bf780a3a4678a5dfd2
Este98 <este98@hotmail.fr>
2025-09-04 08:13:02 +0200

    initialisation du travail js

---
+++ ECF/esteban/devoir_JS/js/script_a_completer.js
@@ -0,0 +1,48 @@
+
+
+
+ //Vérifie si la chaîne contient au moins une lettre minuscule et retourne true ou false
+
+const hasLowercase = (str) => {
+    // a completer
+};
+
+
+//Vérifie si la chaîne contient au moins une lettre majuscule et retourne true ou false
+const hasUppercase = (str) => {
+    // a completer
+};
+
+//Vérifie si la chaîne contient au moins un chiffre et retourne true ou false
+const hasNumber = (str) => {
+    // a completer
+};
+
+
+//Verifie si la chaîne a une longueur minimale et retourne true ou false
+const hasMinLength = (str, length) => {
+    // a completer
+};
+
+
+//Ajoute ou supprime la classe 'checked' sur un élément en fonction de l'état isChecked
+const toogleClassChecked = (node, isChecked ) => {
+
+  // a completer
+}
+
+
+// Écoute des saisies dans le champ du mot de passe et mofication des critères
+passwordElt.addEventListener('input', (e) => {
+   // a completer
+});
+
+
+// Écoute de la soumission du formulaire
+form.addEventListener('submit', (e) => {
+  // a completer
+});
+
+
+
+
