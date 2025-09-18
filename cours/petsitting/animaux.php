575d012897cdcb72969243e9b5e8b0f29c4450cd
david <wawawaformation@gmail.com>
2025-09-18 08:22:31 +0200

    release : squellette page animaux

---
+++ cours/petsitting/animaux.php
@@ -0,0 +1,35 @@
+<?php
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Animaux | petsitting </title>
+    <link rel="stylesheet" href="public/styles.css">
+</head>
+<body>
+    <header id="header">
+        <a>Animaux</a>
+        <nav>
+            <ul class="menu menu-principal">
+                <li class="item"><a href="#">Les especes</a></li>
+                <li class="item"><a href="#">Les Animaux</a></li>
+                <li class="item"><a href="#">Les réservations</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Liste des animaux</h1>
+    </main>
+    <footer id="footer">
+    </footer>
+</body>
+</html>
--- cours/petsitting/model/pdo.php
+++ cours/petsitting/model/pdo.php
@@ -4,7 +4,7 @@
  * objet pdo : connexion à la base de données
  */
 try{
-    $pdo = new PDO('mysql:host=localhost; dbname=petsitting','root','');
+    $pdo = new PDO('mysql:host=localhost; dbname=petsitting','petsitting','1234');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 }catch(Exception $e){
