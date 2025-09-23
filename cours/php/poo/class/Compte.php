dd0f3eb48de9b887f2a009c05b138e2fcfcad984
david <wawawaformation@gmail.com>
2025-09-23 08:51:09 +0200

    une classe et ses propriétés

---
+++ cours/php/poo/class/Compte.php
@@ -0,0 +1,11 @@
+<?php
+
+
+class Compte
+{
+    // les proriétés
+    public string $numero;
+    public int $solde;
+
+    // les méthodes
+}
--- cours/php/poo/index.php
+++ cours/php/poo/index.php
@@ -7,3 +7,25 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+
+
+require_once __DIR__ . '/class/Compte.php';
+
+$khalil = new Compte();
+$khalil->numero = "FR001";
+$khalil->solde = 500;
+
+
+$ayfer = new Compte();
+$ayfer->solde = 100000;
+
+
+
+echo '<pre>';
+var_dump($khalil, $ayfer);
+echo '</pre>';
+
+echo $ayfer->solde . ' euros';
+
+
