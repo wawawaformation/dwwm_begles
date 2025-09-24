dbf840e203faed66fd2ed8d6b1d164d71f0bd796
david <wawawaformation@gmail.com>
2025-09-24 08:06:59 +0200

    debut mercredi - POO

---
+++ cours/php/poo/class/Debug.php
@@ -0,0 +1,52 @@
+<?php
+
+class Debug
+{
+
+    /**
+     * Affiche correctement un tableau
+     * @param array $array
+     * @return void
+     */
+    public static function print_r(array $array): void{
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
+
+
+    /**
+     * Affiche correctement une variable
+     * @param mixed $var
+     * @return void
+     */
+    public static function dump(mixed $var): void
+    { 
+        echo '<pre>';
+        var_dump($var);
+        echo '</pre>';
+    }
+
+
+    /**
+     * Affiche correctement une variable et meurt
+     * @param mixed $var
+     * @return never
+     */
+    public static function dd(mixed $var): void
+    {
+        self::dump($var);
+        die;
+    }
+}
+
+
+//test
+
+$array = ['sylvie', 'bertrand', 'jean-michou'];
+
+Debug::print_r($array);
+
+Debug::dd('pissenli');
+
+echo 'bleblzedfez';
---
+++ cours/php/poo/exercice4.md
@@ -0,0 +1,7 @@
+# la classe Debug
+
+a deux methodes statiques :
+- Debug::print_r() permet de bien afficher print_r
+- Debug::var_dump() permet de bien afficher var_dump
+
+ 
