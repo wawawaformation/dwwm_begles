f4a812ce0206717d2220b9bfaa7961beb034c729
david <wawawaformation@gmail.com>
2025-09-29 11:16:43 +0200

    Animal test unitaire

---
+++ cours/php/animaux/tests/AnimalTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phpunit\Framework\TestCase;
+use App\Model\Entity\Animal;
+
+
+class AnimalTest extends TestCase
+{
+    function testAnimalRetourneIdEtNom(){
+        $animal  = new Animal();
+        $animal->setId(2);
+        $animal->setNom('David');
+
+        $this->assertSame(2, $animal->getId());
+        $this->assertSame('david', $animal->getNom());
+    }
+
+    function testIdEnLectureSeule(){
+        $animal  = new Animal();
+        $animal->setId(2);
+
+
+        $this->expectException(LogicException::class);
+    
+        $animal->setId(3); // on essaye de modifier l'id
+
+    }
+
+
+    function testHydrateAvecAttributInconnu()
+    {
+        $animal = new Animal();
+
+        $animal->hydrate(['nom'=>'David']);
+
+        $this->assertSame('David', $animal->getNom());
+
+       
+
+        $this->expectException(InvalidArgumentException::class);
+        $animal->hydrate(['crie', 'Format document']);
+    }
+}
